eea3237c86e664dcab92e15cdbbb2175357bc9c0
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2023-06-20 18:19:17 -0500

    Se rehizo el código

--- proyecto.py
+++ proyecto.py
@@ -1,55 +1,85 @@
-import heapq
+def dijstra(grafo, nodo_inicial):
+    # En este caso se utilizará set() porque tiene un O(1) promedio para:
+    # agregar add()
+    # eliminar remove() o discard()
+    # buscar elementos in
 
-def dijkstra(grafo, inicio):
-    camino = {nodo: (float('inf'), float('inf'), float('inf')) for nodo in grafo}
-    # (tiempo acumulado, costo acumulado, valor de peso acumulado)
-    camino[inicio] = (0, 0, 0)  
-    # Cola de prioridad
-    pq = [(0, inicio)]
+    # Variable en la que almacenan los nodos analizados
+    nodos_visitados = set()
+    # Variable en la que se almacenan los nodos que no se han analizado
+    nodos_no_visitados = set(i for i in grafo)
+    # Variable que va a almacenar la información de las rutas, al inicio no sabemos nada, por eo los inf
+    # tabla = {nodo: [[tiempo, costo, valor], anterior camino], ... }
+    tabla = {nodo_no_visitado: [[float("inf"), float("inf"), float("inf")], None] for nodo_no_visitado in nodos_no_visitados}
+    tabla[nodo_inicial] = [[0, 0, 0], None]
+
+    # Nodo en el que se encuentra en este momento
+    nodo_actual = nodo_inicial
+    tiempo_acumulado = 0
+    costo_acumulado = 0
+    valor_acumulado = 0
+
+    # Recorrer
+    for i in range(len(nodos_no_visitados)):
+        # Recorro los vecinos del nodo actual
+        for j in grafo[nodo_actual]:
+            # Comparo si la distancia actual es menor que la de la tabla
+            if grafo[nodo_actual][j][2] < tabla[j][0][2] and j in nodos_no_visitados:
+                tiempo_acumulado = tabla[nodo_actual][0][0] + grafo[nodo_actual][j][0]
+                costo_acumulado = tabla[nodo_actual][0][1] + grafo[nodo_actual][j][1]
+                valor_acumulado = tabla[nodo_actual][0][2] + grafo[nodo_actual][j][2]
+                if valor_acumulado < tabla[j][0][2]:
+                    tabla[j] = [[tiempo_acumulado, costo_acumulado, valor_acumulado], nodo_actual]
+
+        nodos_no_visitados.remove(nodo_actual)
+        nodos_visitados.add(nodo_actual)
+
+
+        # Debo buscar en tabla el nodo no visitado con el valor más pequeño     
+        # Esto sir ve para seleccionar al siguiente nodo
+        menor = float("inf")
+        nodo_menor = ""
+        for k in nodos_no_visitados:
+            if menor > tabla[k][0][2]:
+                menor = tabla[k][0][2]
+                nodo_menor = k
+        nodo_actual = nodo_menor
+
+
+    return tabla
+
     
-    while pq:
-        camino_actual, nodo_actual = heapq.heappop(pq)
-        tiempo_actual, costo_actual, valor_estimado = camino[nodo_actual]
-        
-        # Si el nodo ya ha sido visitado, se omite
-        if camino_actual > camino[nodo_actual][2]:
-            continue
-        
-        for vecino, (tiempo, costo, estimado) in grafo[nodo_actual].items():
-            total_tiempo = tiempo_actual + tiempo
-            total_costo = costo_actual + costo
-            total_estimado = valor_estimado + estimado
-            # Si se encuentra un camino más corto hacia el vecino, se actualiza la distancia
-            if (total_estimado) < camino[vecino][2]:
-                camino[vecino] = (total_tiempo, total_costo, total_estimado)
-                heapq.heappush(pq, (total_estimado, vecino))
-    
-    return camino
+
 
 # Ejemplo de uso:
-
 # Creo el grafo de ejemplo
 grafo = {
-    'A': {'B': (5, 0, 5), 'C': (2, 0, 2)},
-    'B': {'D': (3, 2, 5), 'A': (5, 0, 5), 'C': (1, 0, 1)},
-    'C': {'B': (1, 0, 1), 'D': (6, 4, 10), 'A': (2, 0, 2)},
-    'D': {'E': (8, 5, 13), 'C': (6, 4, 10), 'B': (3, 2, 5)},
-    'E': {'D': (8, 5, 13)}
+    'A': {'B': (1, 1, 2), 'D': (0, 8, 8)},
+    'B': {'E': (3, 3, 6), 'D': (1, 4, 5), 'A': (1, 1, 2)},
+    'C': {'E': (6, 3, 9), 'F': (1, 2, 3)},
+    'D': {'F': (1, 1, 2), 'E': (0, 3, 3), 'B': (1, 4, 5), 'D': (0, 8, 8)},
+    'E': {'F': (0, 1, 1), 'C': (6, 3, 9), 'B': (3, 3, 6), 'D': (0, 3, 3)},
+    "F": {'E': (0, 1, 1), 'C': (1, 2, 3), 'D': (1, 1, 2)}
 }
 
 #Nodo de partida
-inicio_nodo = 'A'
+nodo_inicial = 'A'
+destino = "C"
+aux_destino = destino
+camino = dijstra(grafo, nodo_inicial)
+ruta = ""
+while True:
+    ruta += aux_destino + " >-- "
+    aux_destino = camino[aux_destino][1]
+    if aux_destino == nodo_inicial:
+        ruta += nodo_inicial
+        ruta = ruta[::-1]
+        break
 
-camino = dijkstra(grafo, inicio_nodo)
-
-
-print(camino)
-# Imprimimos los caminos que cumplen el nodo de origen, imprimo todos para analizar, luego se cambiará
-for nodo, (tiempo, costo, valor) in camino.items():
-    print(f"Distancia más corta desde {inicio_nodo} hasta {nodo}: tiempo={tiempo}, costo={costo}, valor={valor}")
-
-
-
+print(f"La ruta óptima según nuestra necesidad es: {ruta}")
+print(f"Esta ruta demora: {camino[destino][0][0]}")
+print(f"Esta ruta tiene un costo de: {camino[destino][0][1]}")
+    
 
 
 #while True:
